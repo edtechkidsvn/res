575a32852474bc1cb3dc59c48197869ae3791181
qhuydtvt <qhuydtvt@gmail.com>
2019-01-21 22:27:03 +0700

    .

--- server/routes.ts
+++ server/routes.ts
@@ -2,8 +2,10 @@ import { Application } from 'express';
 import examplesRouter from './api/controllers/examples/router';
 import usersRouter from './api/controllers/users/router';
 import groupsRouter from './api/controllers/groups/router';
+import cors from 'cors';
 
 export default function routes(app: Application): void {
+  app.use(cors());
   app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/users', usersRouter);
   app.use('/api/v1/groups', groupsRouter);
